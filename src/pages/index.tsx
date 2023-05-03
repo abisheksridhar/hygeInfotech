5f51ddee7d5c4794e2f3880749a55c458fab283c
S <abishekanathan.s@capgemini.com>
2023-05-04 02:41:17 +0530

    new changes

---
+++ src/pages/index.tsx
@@ -0,0 +1,24 @@
+import React, { Fragment, useEffect } from 'react'
+import Head from 'next/head'
+import { Footer, NavbarComponent,Preface } from '@components'
+
+
+const Home = () => {
+  return (
+    <Fragment>
+        <Head>
+            <title>Hyge</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href="/logo.PNG" />
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins|Monteserrat"></link>
+        </Head>
+        <main style={{overflowX:'hidden'}}>
+            <NavbarComponent />
+            <Preface />
+            <Footer />
+        </main>
+    </Fragment>
+  )
+}
+
+export default Home
